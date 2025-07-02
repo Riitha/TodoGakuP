01925c0959302e68c09bd0404a6bb93b10865991
Riitha <myuuta29.00@gmail.com>
2025-07-03 00:10:00 +0700

    feat: add progress radius & character icon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import normalImg from './assets/normal.png';
+import taskImg from './assets/tochuu.png';
+import doneImg from './assets/gj.png';
 
 function App() {
   const [todos, setTodos] = useState([])
@@ -7,6 +10,11 @@ function App() {
   const [text, setText] = useState('');
   //end text form
   const url = "http://localhost:3000/todos"
+  //progress bar useState
+  const total = todos.length;
+  const done = todos.filter(todo => todo.status).length;
+  const progress = total === 0 ? 0 : Math.round((done / total) * 100);
+  //end progress bar useState
   // fetch todos
   async function FetchTodos() {
     try {
@@ -18,7 +26,7 @@ function App() {
       });
       const data = await res.json();
       setTodos(data)
-    } catch(error) {
+    } catch (error) {
       console.error('error', error);
     }
   }
@@ -57,7 +65,7 @@ function App() {
       await fetch(`${url}/${id}`, {
         method: 'DELETE'
       });
-      const updateTodos = todos.filter(e => e.id !==id);
+      const updateTodos = todos.filter(e => e.id !== id);
       setTodos(updateTodos);
     } catch (error) {
       console.error('error', error);
@@ -68,30 +76,48 @@ function App() {
   //handle toggle
   async function handleToggleStatus(id, currentStatus) {
     try {
-      const res = await fetch (`${url}/${id}`, {
+      const res = await fetch(`${url}/${id}`, {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          status:!currentStatus
+          status: !currentStatus
         })
       });
       const updated = await res.json();
 
-      const checkTodos = todos.map(todo=> todo.id === id? updated : todo);
+      const checkTodos = todos.map(todo => todo.id === id ? updated : todo);
       setTodos(checkTodos);
     } catch (error) {
       console.error("error", error)
     }
   }
-
+  //handle chara icon
+  function getCharacter(progress) {
+    if (progress === 0) {
+      return normalImg;
+    } else if (progress < 100) {
+      return taskImg;
+    } else {
+      return doneImg;
+    }
+  }
+  //end chara icon
   //use effect
-  useEffect (()=>{
+  useEffect(() => {
     FetchTodos();
   }, []);
   return (
     <>
+      <div className="avatar">
+        <div className="ring-primary ring-offset-base-100 w-24 rounded-full ring-2 ring-offset-2">
+          <img 
+          src= {getCharacter(progress)}
+          alt='temari.png'
+          />
+        </div>
+      </div>
       <h1>Daftar Task</h1>
       <form onSubmit={handleSubmit}>
         <input
@@ -100,21 +126,26 @@ function App() {
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
-      <button type='submit'>add</button>
+        <button type='submit'>add</button>
       </form>
       <ul>
-      {todos.map(todo => (
-        <li key={todo.id}>
-          <input
-          type='checkbox'
-          checked={todo.status}
-          onChange={() => handleToggleStatus(todo.id, todo.status)}
-          />
-        {todo.task} {todo.status ? '✅' : ''}
-        <button onClick={() => handleDelete(todo.id)}>del</button>
-        </li>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <input
+              type='checkbox'
+              checked={todo.status}
+              onChange={() => handleToggleStatus(todo.id, todo.status)}
+            />
+            {todo.task} {todo.status ? '✅' : ''}
+            <button onClick={() => handleDelete(todo.id)}>del</button>
+          </li>
         ))}
       </ul>
+      <div
+        className="radial-progress bg-primary text-primary-content border-primary border-4"
+        style={{ "--value": progress }} aria-valuenow={progress} role="progressbar">
+        {progress}%
+      </div>
     </>
   )
 }
